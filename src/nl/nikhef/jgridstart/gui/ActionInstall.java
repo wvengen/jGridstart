9b717cc637fabd7647227acdc5df81f021ec3fb1
wvengen <wvengen+jgridstart@nikhef.nl>
2009-05-04 15:19:50 +0000

    remove conflicting shortcut key for install action; rework exception handling for install action

--- src/nl/nikhef/jgridstart/gui/ActionInstall.java
+++ src/nl/nikhef/jgridstart/gui/ActionInstall.java
@@ -3,7 +3,6 @@ package nl.nikhef.jgridstart.gui;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -11,7 +10,6 @@ import java.security.cert.CertificateException;
 
 import javax.swing.AbstractAction;
 import javax.swing.JFrame;
-import javax.swing.KeyStroke;
 import javax.swing.Timer;
 
 import nl.nikhef.jgridstart.CertificatePair;
@@ -26,49 +24,41 @@ public class ActionInstall extends CertificateAction {
 	super(parent, s);
 	putValue(NAME, "Install...");
 	putValue(MNEMONIC_KEY, new Integer('I'));
-	putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke("control I"));
 	BareBonesActionLaunch.addAction("install", this);
     }
     
     public void actionPerformed(ActionEvent e) {
+	CertificatePair cert = selection.getCertificatePair();
+	// TODO explain what'll happen in password dialog or option pane
 	try {
-	    CertificatePair cert = selection.getCertificatePair();
-	    // TODO explain what'll happen in password dialog or option pane
-	    // export to pkcs#12 certificate
 	    final TempFileWriter pkcs = new TempFileWriter("browser", ".p12");
-	    try {
-		cert.exportTo(new File(pkcs.getPath()));
-	    } catch (KeyStoreException e1) {
-		e1.printStackTrace();
-	    } catch (NoSuchProviderException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	    } catch (NoSuchAlgorithmException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	    } catch (CertificateException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	    }
-	    // import into browser
-	    BareBonesActionLaunch.openURL(
-	    	new File(pkcs.getPath()).toURI().toURL(),
-	    	parent);
 	    // delete after a timeout
 	    // TODO figure out reasonable timeout
-	    /*
-	    new Timer(1*60*1000, new AbstractAction() {
+	    new Timer(1*30*1000, new AbstractAction() {
 		public void actionPerformed(ActionEvent e) {
-		    pkcs.delete();
+		    if (pkcs!=null) pkcs.delete();
 		}
 	    });
-	    */
-	} catch (MalformedURLException e1) {
+	    cert.exportTo(new File(pkcs.getPath()));
+	    // import into browser
+	    // TODO better export; maybe import info.ziyan.net.httpserver and supply mime-type
+	    BareBonesActionLaunch.openURL(
+		    new File(pkcs.getPath()).toURI().toURL(),
+		    parent);
+	} catch (KeyStoreException e1) {
+	    e1.printStackTrace();
+	} catch (NoSuchProviderException e1) {
 	    // TODO Auto-generated catch block
 	    e1.printStackTrace();
-	} catch (IOException e2) {
+	} catch (NoSuchAlgorithmException e1) {
 	    // TODO Auto-generated catch block
-	    e2.printStackTrace();
+	    e1.printStackTrace();
+	} catch (CertificateException e1) {
+	    // TODO Auto-generated catch block
+	    e1.printStackTrace();
+	} catch (IOException e1) {
+	    // TODO Auto-generated catch block
+	    e1.printStackTrace();
 	}
     }
 }
