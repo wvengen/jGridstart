dc88bc8f7f3b1c62596de53a6391e05d67a9f318
wvengen <wvengen+jgridstart@nikhef.nl>
2009-05-06 09:14:35 +0000

    add internal error dialog

--- src/nl/nikhef/jgridstart/gui/util/ErrorMessage.java
+++ src/nl/nikhef/jgridstart/gui/util/ErrorMessage.java
@@ -7,7 +7,8 @@ import javax.swing.JOptionPane;
  * Small class to present an error message to the user
  */
 public class ErrorMessage {
-    /** show an error to the user
+    /** show an error to the user. This method is for errors that are
+     * meaningful to the user, such as an IOException.
      * 
      * @param parent Parent window
      * @param title Title of the dialog
@@ -16,5 +17,20 @@ public class ErrorMessage {
     public static void error(JFrame parent, String title, Exception e) {
 	JOptionPane.showMessageDialog(parent, e.getMessage(),
 		title, JOptionPane.ERROR_MESSAGE);
+    }
+    
+    /** show an error to the user. This method is for errors that should
+     * not occur; the dialog indicates that the user can contact
+     * the developers.
+     * 
+     * @param parent Parent window
+     * @param e Exception to get information from
+     */
+    public static void internal(JFrame parent, Exception e) {
+	String s = "I'm sorry to report that an unexpected internal error occured.\n"
+	          +"Please contact technical support for help.\n";
+	// TODO include contact details for technical support
+	JOptionPane.showMessageDialog(parent, s+e.getMessage(),
+		"Internal problem", JOptionPane.ERROR_MESSAGE);
     }
 }
