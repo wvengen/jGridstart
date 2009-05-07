c318e1cdcd4c171ff575e38e2f18911e15f5e748
wvengen <wvengen+jgridstart@nikhef.nl>
2009-05-07 10:40:26 +0000

    update shellscript to pass script name to java application using environment

--- jgridstart.sh
+++ jgridstart.sh
@@ -2,6 +2,7 @@
 export BASE=`dirname $0`
 export LIB="$BASE/thirdparty"
 export CLASSPATH="$LIB/bouncycastle/bcprov-jdk14-142.jar:$LIB/commons-cli/commons-cli-1.2.jar:$LIB/flyingsaucer/core-renderer-minimal.jar:$BASE/bin"
+export INVOKED_PROGRAM="$0"
 if [ "$DISPLAY" ]; then
 	java nl.nikhef.jgridstart.gui.Main $@
 else
