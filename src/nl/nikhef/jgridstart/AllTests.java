eee51ebf2bc96438fc470bba6ff3aeb432a2adac
wvengen <wvengen+jgridstart@nikhef.nl>
2009-06-02 12:45:33 +0000

    more testing; the focus tests may fail at Mac OS X and Linux without click-to-focus :(

--- src/nl/nikhef/jgridstart/AllTests.java
+++ src/nl/nikhef/jgridstart/AllTests.java
@@ -1,6 +1,8 @@
 package nl.nikhef.jgridstart;
 
 import nl.nikhef.jgridstart.util.PasswordCacheTest;
+import nl.nikhef.xhtmlrenderer.swing.TemplateDocumentTest;
+import nl.nikhef.xhtmlrenderer.swing.TemplatePanelTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -11,6 +13,8 @@ public class AllTests {
 	TestSuite suite = new TestSuite("Test for nl.nikhef.jgridstart");
 	//$JUnit-BEGIN$
 	suite.addTestSuite(PasswordCacheTest.class);
+	suite.addTestSuite(TemplateDocumentTest.class);
+	suite.addTestSuite(TemplatePanelTest.class);
 	//$JUnit-END$
 	return suite;
     }
