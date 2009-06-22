9e3d47b9b284fb86a72de8c13791fcbdf0c59c50
wvengen <wvengen+jgridstart@nikhef.nl>
2009-06-22 08:51:03 +0000

    proper cmdline args for batch file

--- jgridstart.bat
+++ jgridstart.bat
@@ -5,7 +5,7 @@ REM
 
 set BASE=%~dp0
 set LIB=%BASE%\thirdparty
-set CLASSPATH=%LIB%\bouncycastle\bcprov-jdk14-143.jar;%LIB%\commons-cli\commons-cli-1.2.jar;%LIB%\flyingsaucer\core-renderer-minimal.jar;%LIB%\flyingsaucer\iText-2.0.8.jar;%LIB%\swingworker\swing-worker-1.2.jar;%BASE%\winregistry\WinRegistry-3.4.jar;%BASE%\bin
+set CLASSPATH=%LIB%\bouncycastle\bcprov-jdk14-143.jar;%LIB%\commons-cli\commons-cli-1.2.jar;%LIB%\flyingsaucer\core-renderer-minimal.jar;%LIB%\flyingsaucer\iText-2.0.8.jar;%LIB%\swingworker\swing-worker-1.2.jar;%LIB%\winregistry\WinRegistry-3.4.jar;%BASE%\bin
 set INVOKED_PROGRAM=%0
 
 REM undocumented behaviour: first parameter can be "test" to run unit tests,
@@ -24,17 +24,17 @@ if not "%1"=="test" goto :notest
 :notest
 if not "%1"=="cli" goto :nowantcli
 	shift
-	java nl.nikhef.jgridstart.cli.Main $@
+	java nl.nikhef.jgridstart.cli.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 	goto :end
 
 :nowantcli
 if not "%1"=="gui" goto :nowantgui
 	shift
-	java nl.nikhef.jgridstart.gui.Main $@
+	java nl.nikhef.jgridstart.gui.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 	goto :end
 
 :nowantgui
-java nl.nikhef.jgridstart.gui.Main $@
+java nl.nikhef.jgridstart.gui.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto :end
 
 :end
