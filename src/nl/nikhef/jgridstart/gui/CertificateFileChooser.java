6bfe033c89182afdcb097dfec2ea524ac844a0a4
wvengen <wvengen+jgridstart@nikhef.nl>
2009-07-21 14:14:18 +0000

    make sure a default filefilter is selected (needed for Mac OS X)

--- src/nl/nikhef/jgridstart/gui/CertificateFileChooser.java
+++ src/nl/nikhef/jgridstart/gui/CertificateFileChooser.java
@@ -2,7 +2,9 @@ package nl.nikhef.jgridstart.gui;
 
 import java.io.File;
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 
+import nl.nikhef.jgridstart.gui.util.ErrorMessage;
 import nl.nikhef.jgridstart.gui.util.FileFilterSuffix;
 
 /** Filechooser for PEM and PKCS#12 files */
@@ -12,14 +14,17 @@ public class CertificateFileChooser extends JFileChooser {
 	super();
 	
 	addChoosableFileFilter(
-		new FileFilterSuffix("PEM Certificate Bundle", ".pem"));
-	addChoosableFileFilter(
 		new FileFilterSuffix("PKCS#12 Certificate Bundle", ".p12,.pfx"));
+	addChoosableFileFilter(
+		new FileFilterSuffix("PEM Certificate Bundle", ".pem"));
 	if (open)
 	    addChoosableFileFilter(
 		    new FileFilterSuffix("Certificate Bundle", ".p12,.pfx,.pem"));
 	
 	setAcceptAllFileFilterUsed(false);
+	
+	// Make sure a filefilter is selected; needed on Mac OS X only
+	setFileFilter(getChoosableFileFilters()[0]);
 	
 	setFileSelectionMode(FILES_ONLY);
 	setDialogType(open ? OPEN_DIALOG : SAVE_DIALOG);
@@ -35,7 +40,8 @@ public class CertificateFileChooser extends JFileChooser {
 	File f = super.getSelectedFile();
 	if (f!=null && getDialogType() == SAVE_DIALOG) {
 	    if (!getFileFilter().accept(f)) {
-		f = new File(f.getPath() + ((FileFilterSuffix)getFileFilter()).getExtensions()[0] );
+		FileFilter ff = getFileFilter();
+		f = new File(f.getPath() + ((FileFilterSuffix)ff).getExtensions()[0] );
 	    }
 	}
 	return f;
