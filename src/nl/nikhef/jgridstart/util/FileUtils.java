89ac4720b34171cccd7f4c8e9711bf18cab76131
wvengen <wvengen+jgridstart@nikhef.nl>
2009-08-31 12:50:44 +0000

    make CopyFile work on Vista as well (use robocopy instead of xcopy when possible)

--- src/nl/nikhef/jgridstart/util/FileUtils.java
+++ src/nl/nikhef/jgridstart/util/FileUtils.java
@@ -21,14 +21,29 @@ public class FileUtils {
     public static boolean CopyFile(File in, File out) throws IOException {
 	String[] cmd;
 	if (System.getProperty("os.name").startsWith("Windows")) {
-	    // windows: first touch the destination, then xcopy. If the file
-	    // doesn't exist on copying, xcopy will ask whether you want to
-	    // create it as a directory or just copy a file, so we always
-	    // just put "F" in xcopy's stdin.
-	    cmd = new String[]{"xcopy.exe",
+	    // windows: use special copy program to retain permissions.
+	    //   on Vista, "xcopy /O" requires administrative rights, so we
+	    //   have to resort to using robocopy there.
+	    try {
+		cmd = new String[] { "robocopy.exe" };
+		int ret = Runtime.getRuntime().exec(cmd).waitFor();
+		if (ret!=0 && ret!=16) throw new InterruptedException();
+		// we have robocopy
+		cmd = new String[]{"robocopy.exe",
+			    in.getAbsolutePath(),
+			    out.getAbsolutePath(),
+			    "/COPYALL", "/NP"};
+	    } catch (InterruptedException e) {
+		// use xcopy instead
+		cmd = new String[]{"xcopy.exe",
 		    in.getAbsolutePath(),
 		    out.getAbsolutePath(),
 		    "/O", "/Q", "/Y"};
+	    }
+	    
+	    // If the file/ doesn't exist on copying, xcopy will ask whether you want
+	    // to create it as a directory or just copy a file, so we always
+	    // just put "F" in xcopy's stdin.
 	    return Exec(cmd, "F", null) == 0;
 	} else {
 	    // other, assume unix-like
