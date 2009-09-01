1a7d2f6c3bf109ee7e808d90e0a6195ceac37567
wvengen <wvengen+jgridstart@nikhef.nl>
2009-09-01 12:44:49 +0000

    initial version of gui test program; also create build target

--- src/nl/nikhef/jgridstart/AllTests.java
+++ src/nl/nikhef/jgridstart/AllTests.java
@@ -1,5 +1,9 @@
 package nl.nikhef.jgridstart;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import nl.nikhef.jgridstart.gui.util.TemplateButtonPanelTest;
 import nl.nikhef.jgridstart.install.BrowsersMacOSXTest;
 import nl.nikhef.jgridstart.util.PasswordCacheTest;
@@ -7,6 +11,7 @@ import nl.nikhef.xhtmlrenderer.swing.TemplateDocumentTest;
 import nl.nikhef.xhtmlrenderer.swing.TemplatePanelTest;
 
 import junit.framework.Test;
+import junit.framework.TestResult;
 import junit.framework.TestSuite;
 
 public class AllTests {
@@ -23,5 +28,41 @@ public class AllTests {
 	//$JUnit-END$
 	return suite;
     }
+    
+    /** GUI test program that runs unit tests */
+    public static void main(String[] args) {
+	// initial user message
+	int ret = JOptionPane.showConfirmDialog(null,
+		"Thank you for running the jGridstart testing program. Your cooperation enables us to\n" +
+		"improve the software. After you press Ok, please wait while the tests are running,\n" +
+		"until a message appears about the tests being done.",
+		"jGridStart Testing Program", JOptionPane.OK_CANCEL_OPTION);
+	if (ret!=JOptionPane.OK_OPTION) return;
+	
+	// TODO redirect messages to output
+	
+	// run tests
+	JFrame frame = new JFrame("jGridstart Testing Program");
+	frame.add(new JLabel("     Please don't touch your computer until testing is complete...     "));
+	frame.setSize(400, 250);
+	frame.setVisible(true);
+	TestResult result = junit.textui.TestRunner.run(suite());
+	frame.setVisible(false);
+	
+	// generate report, ask user confirmation
+	// TODO generate report and submit information
+	JOptionPane.showMessageDialog(null,
+		"The tests have been run, resulting in " + result.errorCount() + " failures.\n" +
+		"\n" +
+		"Thank you for your help.",
+		"jGridStart Testing Program", JOptionPane.OK_OPTION|JOptionPane.INFORMATION_MESSAGE);
+	System.exit(0);
+    }
+
+    /*
+	"jGridstart version: "+System.getProperty("jgridstart.version")+ 
+	" (rev "+System.getProperty("jgridstart.revision")+")\n" +
+	"Java runtime environment version: "+System.getProperty("java.version"),
+     */
 
 }
