05b8f9de77117ff2d5f8854d89b81ea7551c9190
wvengen <wvengen+jgridstart@nikhef.nl>
2009-11-27 16:06:02 +0000

    fix browsertool script classpath

--- browsertool.sh
+++ browsertool.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 BASE=`dirname $0`
-CLASSPATH=$BASE/thirdparty/commons/commons-cli-1.2.jar:$BASE/bin
+LIB="$BASE/thirdparty"
+CLASSPATH=$LIB/commons/commons-lang-2.4.jar:$LIB/commons/commons-cli-1.2.jar:$BASE/bin
 INVOKED_PROGRAM="$0"
 export CLASSPATH INVOKED_PROGRAM COLUMNS
 
