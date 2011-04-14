b3328b774557d3732467d2328dc8dd60fe8b1e81
wvengen <wvengen+jgridstart@nikhef.nl>
2011-04-15 00:50:03 +0300

    update browsertool scripts

--- browsers/browsertool.sh
+++ browsers/browsertool.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-
-if [ ! `which mvn` ]; then
-	echo "You need to install maven first." >/dev/stderr
-	echo "  http://maven.apache.org/" >/dev/stderr
+if ! mvn -v >/dev/null 2>&1; then
+	echo "You need to install maven first: http://maven.apache.org/" >/dev/stderr
 	exit 1
 fi
-
 mvn -B -q exec:java -Dexec.mainClass="nl.nikhef.browsers.BrowserTool" -Dexec.args="$@"
-
