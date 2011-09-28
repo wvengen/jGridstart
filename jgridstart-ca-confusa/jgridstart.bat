802cb81e200258c1c4f2e342009f7e79cb9553ba
wvengen <wvengen+jgridstart@nikhef.nl>
2011-09-28 15:57:02 +0200

    add jgridstart run scripts for ca packages

---
+++ jgridstart-ca-confusa/jgridstart.bat
@@ -0,0 +1,1 @@
+mvn -B -q exec:java -Dexec.mainClass="nl.nikhef.jgridstart.gui.Main" -Dexec.args="$@" -Djgridstart.requestwizard.provider=nl.nikhef.jgridstart.ca.confusa.RequestWizard
---
+++ jgridstart-ca-confusa/jgridstart.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if ! mvn -v >/dev/null 2>&1; then
+	echo "You need to install maven first: http://maven.apache.org/" >/dev/stderr
+	exit 1
+fi
+
+mvn -B -q exec:java -Dexec.mainClass="nl.nikhef.jgridstart.gui.Main" -Dexec.args="`echo $@`" -Djgridstart.requestwizard.provider=nl.nikhef.jgridstart.ca.confusa.RequestWizard
