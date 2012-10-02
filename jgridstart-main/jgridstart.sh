167f96dce613da02c409dfbc8fa95a0ebf05c223
wvengen <wvengen+jgridstart@nikhef.nl>
2012-10-02 15:10:36 +0200

    add -screenshots argument to jgridstart.{sh,bat} and fix lineendings in batchfile :)

--- jgridstart-main/jgridstart.sh
+++ jgridstart-main/jgridstart.sh
@@ -5,4 +5,11 @@ if ! mvn -v >/dev/null 2>&1; then
 	exit 1
 fi
 
+if [ "$1" = "-screenshots" ]; then
+	shift
+	mvn -B -q exec:java -Dexec.mainClass="nl.nikhef.jgridstart.gui.util.ScreenshotsGuitest" -Dexec.classpathScope="test" -Dexec.args="`echo $@`"
+	exit $?
+fi
+
 mvn -B -q exec:java -Dexec.mainClass="nl.nikhef.jgridstart.gui.Main" -Dexec.args="`echo $@`"
+exit $?
