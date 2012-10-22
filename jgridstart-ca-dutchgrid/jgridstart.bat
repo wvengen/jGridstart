eb2ae2050fa16d4a604b9f4ae09183f403be40fc
wvengen <wvengen+jgridstart@nikhef.nl>
2012-10-22 13:48:30 +0200

    use proper provider in ca-dutchgrid startup script

--- jgridstart-ca-dutchgrid/jgridstart.bat
+++ jgridstart-ca-dutchgrid/jgridstart.bat
@@ -1,4 +1,4 @@
 @echo off
 REM - if you have your own TestCA instance, you may want to set
 REM SET EXTRAARGS=-Djgridstart.ca.base=http://my.site/testca/
-mvn -B -q exec:java -Dexec.mainClass="nl.nikhef.jgridstart.gui.Main" -Dexec.args="%$" -Djgridstart.requestwizard.provider=nl.nikhef.jgridstart.ca.dutchgrid.RequestWizard %EXTRAARGS%
+mvn -B -q exec:java -Dexec.mainClass="nl.nikhef.jgridstart.gui.Main" -Dexec.args="%$" -Djgridstart.requestwizard.provider=nl.nikhef.jgridstart.ca.dutchgrid.RequestWizard -Djgridstart.ca.provider=nl.nikhef.jgridstart.ca.dutchgrid.DutchGridCA %EXTRAARGS%
--- jgridstart-ca-dutchgrid/jgridstart.sh
+++ jgridstart-ca-dutchgrid/jgridstart.sh
@@ -8,4 +8,4 @@ if ! mvn -v >/dev/null 2>&1; then
 	exit 1
 fi
 
-mvn -B -q exec:java -Dexec.mainClass="nl.nikhef.jgridstart.gui.Main" -Dexec.args="`echo $@`" -Djgridstart.requestwizard.provider=nl.nikhef.jgridstart.ca.dutchgrid.RequestWizard $EXTRAARGS
+mvn -B -q exec:java -Dexec.mainClass="nl.nikhef.jgridstart.gui.Main" -Dexec.args="`echo $@`" -Djgridstart.requestwizard.provider=nl.nikhef.jgridstart.ca.dutchgrid.RequestWizard -Djgridstart.ca.provider=nl.nikhef.jgridstart.ca.dutchgrid.DutchGridCA $EXTRAARGS
